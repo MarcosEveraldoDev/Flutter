f6399fb4aa2b622330b1cf19393f5aa4e118a5c3
Marcos Everaldo Dev <marcosdev5411@gmail.com>
2023-12-05 09:42:47 -0300

    first commit

---
+++ at_1/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:at_1/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
